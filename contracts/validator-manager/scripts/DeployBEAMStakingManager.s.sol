b8908118dfca8295fea47beae076a7a82e3ba488
zjesko <akshat.c2k@gmail.com>
2025-02-26 18:44:50 +0530

    add reward streams setup to deployment

--- contracts/validator-manager/scripts/DeployBEAMStakingManager.s.sol
+++ contracts/validator-manager/scripts/DeployBEAMStakingManager.s.sol
@@ -15,6 +15,7 @@ import {EthereumVaultConnector} from "evc/EthereumVaultConnector.sol";
 import {ProxyAdmin} from "openzeppelin-contracts/proxy/transparent/ProxyAdmin.sol";
 import {TransparentUpgradeableProxy} from "openzeppelin-contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ITransparentUpgradeableProxy} from "openzeppelin-contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {ExampleERC20} from "@mocks/ExampleERC20.sol";
 
 /**
  * @notice Script to deploy and initialize Native721TokenStakingManager with a new proxy
@@ -44,6 +45,7 @@ contract DeployBEAMStakingManager is Script {
     uint256 constant MAXIMUM_NFT_AMOUNT = 1000;
     uint256 constant MINIMUM_DELEGATION_AMOUNT = 1e18;
     bytes32 constant UPTIME_BLOCKCHAIN_ID = bytes32(hex"0d5c3cfbccc694b3d5490a2de71d43d391f6c73dac86ed5169572d7646c7ece2");
+    uint256 constant REWARD_PER_EPOCH = 1000e18;
 
     function run() external {
         // Start broadcasting transactions
@@ -96,6 +98,24 @@ contract DeployBEAMStakingManager is Script {
         bytes32 ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
         address proxyAdmin = address(uint160(uint256(vm.load(address(proxy), ADMIN_SLOT))));
         console.log("ProxyAdmin contract deployed at:", proxyAdmin);
+
+        // example reward
+        ExampleERC20 rewardToken = new ExampleERC20();
+        uint128[] memory amounts = new uint128[](3);
+        amounts[0] = REWARD_PER_EPOCH;
+        amounts[1] = REWARD_PER_EPOCH;
+        amounts[1] = REWARD_PER_EPOCH;
+
+        rewardToken.approve(address(balanceTracker), REWARD_PER_EPOCH * 3);
+        rewardToken.approve(address(balanceTrackerNFT), REWARD_PER_EPOCH * 3);
+
+        balanceTracker.registerReward(address(proxy), address(rewardToken), 0, amounts);
+        balanceTrackerNFT.registerReward(address(proxy), address(rewardToken), 0, amounts);
+        console.log("Registered reward streams:", proxyAdmin);
+
+        // rewards need to be enabled by the user before claiming
+        balanceTracker.enableReward(address(proxy), address(rewardToken));
+        balanceTrackerNFT.enableReward(address(proxy), address(rewardToken));
 
         vm.stopBroadcast();
     }
