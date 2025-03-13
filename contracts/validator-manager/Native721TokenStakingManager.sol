1b50f6429071e77c735df55143a86dda586cf5d4
zjesko <akshat.c2k@gmail.com>
2025-03-14 01:17:10 +0530

    minor fix

--- contracts/validator-manager/Native721TokenStakingManager.sol
+++ contracts/validator-manager/Native721TokenStakingManager.sol
@@ -678,6 +678,8 @@ contract Native721TokenStakingManager is
         for (uint256 i = 0; i < delegations.length; i++) {
             Delegator memory delegator = $._delegatorStakes[delegations[i]];
 
+            // skip if delegation started after this epoch
+            if(delegator.startTime > (epoch + 1) * $._epochDuration){ continue; }
             uint256 delWeight;
             {
                 uint64 delegationStart = uint64(Math.max(delegator.startTime, epoch * $._epochDuration));
